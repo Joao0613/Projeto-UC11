90ddc01dcb43701959afc6d28ddcad01534e4af4
João Victor Borges <138502779+Joao0613@users.noreply.github.com>
2026-02-02 15:12:02 -0300

    Implementada listagem de produtos do banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -49,10 +49,36 @@ public class ProdutosDAO {
 }
 
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
+        String sql = "SELECT * FROM produtos";
+        ArrayList<ProdutosDTO> lista = new ArrayList<>();
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getDouble("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                lista.add(produto);
+            }
+
+            prep.close();
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro);
+        }
+
+        return lista;
     }
+
     
     
     
